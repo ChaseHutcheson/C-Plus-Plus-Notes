da169cdec86a69dd72a3a2d512a936b7f6693e49
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2023-11-17 11:41:01 -0500

    initial Commit

---
+++ 1.) Hello World/helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+// You need semicolons btw.
+int main(){
+
+    // "std" stands for standard, and "cout" is character output. So std::cout is standard character output
+    // "<<" means insertion. so "Hello world" is inserted into the output stream
+    
+    std::cout << "Hello, World. \n";
+    std::cout << "Hello, C++. ";
+
+    return 0; // If 0 is returned, the function ran without issue. if 1, there was an error
+}
